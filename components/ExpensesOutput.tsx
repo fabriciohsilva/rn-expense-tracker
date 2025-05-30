33f4cefc5095112faf9c9a5e968cbc1170a97300
Fabrício Silva <fabricioh.silva@live.com>
2025-05-30 14:49:52 -0300

    styles and navigation initial vertion

---
+++ components/ExpensesOutput.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, View } from "react-native";
+
+import ExpensesList from "./ExpensesOutput/ExpensesList";
+import ExpensesSummary from "./ExpensesOutput/ExpensesSummary";
+
+import { DUMMY_EXPENSES } from "../data/dummy-data";
+import { GlobalStyles } from "../constants/styles";
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
---
+++ screens/AllExpenses.tsx
@@ -0,0 +1,7 @@
+import ExpensesOutput from "../components/ExpensesOutput";
+
+function AllExpenses() {
+  return <ExpensesOutput expensesPeriod="Total" />;
+}
+
+export default AllExpenses;
