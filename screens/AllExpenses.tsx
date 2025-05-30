5fb1f4170b26c4a60975524f1744f742054ee0ec
Fabrício Silva <fabricioh.silva@live.com>
2025-05-30 19:20:55 -0300

    creating expense form and applying validations

--- screens/AllExpenses.tsx
+++ screens/AllExpenses.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import ExpensesOutput from "../components/ExpensesOutput";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 
 function AllExpenses() {
